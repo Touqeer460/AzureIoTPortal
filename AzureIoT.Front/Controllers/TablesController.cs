620a0f23323a33b88b664199193460e4f9019b12
Touqeer Ahmed Siddiqui <touqeer460@live.com>
2019-07-21 07:08:12 -0700

    Add project files.

---
+++ AzureIoT.Front/Controllers/TablesController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AzureIoT.FrontEnd.Controllers
+{
+    public class TablesController : Controller
+    {
+        //
+        // GET: /Table/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Telemetries()
+        {
+            return View();
+        }
+
+        public ActionResult DeviceGroups()
+        {
+            return View();
+        }
+    }
+}
